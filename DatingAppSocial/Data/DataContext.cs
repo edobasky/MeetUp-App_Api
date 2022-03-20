1da54ac0591c7c8c36785b1f843437aa87ce69d1
edobasky <edore7@gmail.com>
2022-03-20 10:13:08 +0100

    initial commit

---
+++ DatingAppSocial/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingAppSocial.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppSocial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> User { get; set; }
+    }
+}
